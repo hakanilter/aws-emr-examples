c303c3fc1de4ea542758b420ea4812486c130e50
Hakan Ilter <hakanilter@gmail.com>
2018-01-18 15:07:55 +0000

    Fixed run-emr-job script

--- scripts/run-emr-job.sh
+++ scripts/run-emr-job.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-CLUSTER_ID="j-123456"
-CLASS_NAME="com.datapyro.emr.spark.SparkS3Aggregation"
-JAR_LOCATION="s3://datapyro-emr/example/app/emr-example-1.0.0-SNAPSHOT.jar"
-INPUT_FOLDER="s3://datapyro-emr/example/data"
-OUTPUT_FOLDER="s3://datapyro-emr/example/output"
+CLUSTER_ID="j-854WGX1R6Z44"
+CLASS_NAME="com.datapyro.emr.spark.SparkS3BinaryData"
+JAR_LOCATION="s3://datapyro-main/lib/aws-emr-examples-1.0.0-SNAPSHOT-dist.jar"
+INPUT_FOLDER="s3://datapyro-main/test"
+OUTPUT_FOLDER="s3://datapyro-main/output"
 
 aws emr add-steps --cluster-id $CLUSTER_ID --steps Type=spark,Name=EmrExample,Args=[--deploy-mode,cluster,--class,$CLASS_NAME,--master,yarn,--conf,spark.yarn.submit.waitAppCompletion=false,$JAR_LOCATION,$INPUT_FOLDER,$OUTPUT_FOLDER],ActionOnFailure=CONTINUE
+
+# aws emr describe-step --cluster-id $CLUSTER_ID --step-id s-CK2QM0DYXS84
